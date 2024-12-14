f1fd1b2a23604e5f6677cd0373e852e8d00cc25d
alauddinDipu <alauddin.cucse@gmail.com>
2024-12-14 23:35:22 +0600

    Done design

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
     return (
         <div>
             <nav>
-                <u>       <b><p>CRUD Operation</p></b>
+                <u>       <b><p><Link href="/">CRUD Operation</Link></p></b>
                 </u>
-                <Link href="/Read">Home Menu</Link>
+                {/* <Link href="/Read">Home Menu</Link> */}
                 <Link href="/CreateOption/Create">Create Menu</Link>
             </nav>
         </div>
--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ import DataList from "./Read/DataList";
 export default function Home() {
   return (
     <main>
-        <h2>Dashboard page</h2>
-        <DataList></DataList>
+      {/* <h2>Dashboard page</h2> */}
+      <DataList></DataList>
     </main>
   )
 }
